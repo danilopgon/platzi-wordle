2b8b4e9ae26e272d6eaa812f3a0f261de688cad6
danilopgon <dani.lopezgonzalez.94@gmail.com>
2024-06-03 18:30:46 +0200

    Add Observer to delete letter

--- src/index.js
+++ src/index.js
@@ -1,20 +1,17 @@
-/**
- * Aplicación de fromEvent en PlatziWordle:
- * fromEvent nos ayuda a atrapar eventos generados por elementos en el DOM.
- */
-
 import { fromEvent } from "rxjs";
 
 const letterRows = document.getElementsByClassName("letter-row");
+
 const onKeyDown$ = fromEvent(document, "keydown");
-let letterIndex = 0;
 let letterRowIndex = 0;
+let letterIndex = 0;
 
 const insertLetter = {
   next: (event) => {
     const pressedKey = event.key.toUpperCase();
+
     if (pressedKey.length === 1 && pressedKey.match(/[a-z]/i)) {
-      let letterBox =
+      const letterBox =
         Array.from(letterRows)[letterRowIndex].children[letterIndex];
       letterBox.textContent = pressedKey;
       letterBox.classList.add("filled-letter");
@@ -23,4 +20,19 @@ const insertLetter = {
   },
 };
 
+const deleteLetter = {
+  next: (event) => {
+    const pressedKey = event.key;
+
+    if (pressedKey === "Backspace") {
+      letterIndex--;
+      const letterBox =
+        Array.from(letterRows)[letterRowIndex].children[letterIndex];
+      letterBox.textContent = "";
+      letterBox.classList.remove("filled-letter");
+    }
+  },
+};
+
 onKeyDown$.subscribe(insertLetter);
+onKeyDown$.subscribe(deleteLetter);
